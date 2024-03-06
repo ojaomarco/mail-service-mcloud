e42adec065d50fc84b14df6eb5b09df2ea84e96d
ojaomarco <ojaomarco@gmail.com>
2024-03-06 14:48:40 -0300

    update auth

--- config/auth.py
+++ config/auth.py
@@ -7,13 +7,6 @@ load_dotenv()
 BASE_URL = 'http://multipetcloud.com.br/api/devices/'
 BASE_URL_ISSUE = 'https://multipetcloud.com.br/api/issue-inspections/'
 
-# AUTH =('ojaomarco', 'Jo@o1501')
-# ELASTIC_AUTH = ('multipet', 'multipet@2022#$')
-
-# SENDER = "multipetmcloud.noreply@gmail.com"
-# PASSWORD = "terf fcom bfii tkyj"
-# text = "Multipet Sopradoras"
-
 AUTH =(os.getenv('MCLOUD_USER'), os.getenv('MCLOUD_PWD'))
 ELASTIC_AUTH = (os.getenv('ELASTIC_USER'), os.getenv('ELASTIC_PWD'))
 
